9979dbc68cdea9939ebb01fea182cd1a2e263e12
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-04-29 12:35:55 +0200

    ajout de les méthode demander

---
+++ compte/app_compte/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace app_compte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         Compte premier_compte = new Compte(1, "Matthieu THUET", 1000, -500);
+        }
+    }
+}
---
+++ compte/conte_library/compte.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace compte_library
+{
+    public class Compte
+    {
+        public int NumeroDeCompte;
+        private string NomDuProprietaireDuCompte;
+        private double SoldeDuCompte;
+        private double DecouvertAutorise;
+
+        public Compte(int numeroDeCompte, string nomDuProprietaireDuCompte, double soldeDuCompte, double decouvertAutorise)
+        {
+            NumeroDeCompte = numeroDeCompte;
+            NomDuProprietaireDuCompte = nomDuProprietaireDuCompte;
+            SoldeDuCompte = soldeDuCompte;
+            DecouvertAutorise = decouvertAutorise;
+        }
+
+        public bool Crediter(double montant)
+        {
+            SoldeDuCompte += montant;
+            return true;
+        }
+
+        public bool Debiter(double montant)
+        {
+            if (SoldeDuCompte-montant>=this.DecouvertAutorise && montant>=0)
+            {
+                SoldeDuCompte -= montant;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Transferer(Compte compteACrediter, double montant)
+        {
+            if (Debiter(montant))
+            {
+                compteACrediter.Crediter(montant);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public override string ToString()
+        {
+            return "ce compte a pour numero" + this.NumeroDeCompte + "il appartien à" + this.NomDuProprietaireDuCompte + "sont solde est de" + this.SoldeDuCompte + "et sont découvert autoriser est de" + this.DecouvertAutorise;
+        }
+    }
+}
